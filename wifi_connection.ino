11320d96350b1701a7096e39df7c153fc5b8191b
Akilan S <akil75552@gmail.com>
2026-07-05 14:37:30 +0530

    Create wifi_connection.ino

---
+++ wifi_connection.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi Connected Successfully!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi Connected");
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(5000);
+}
